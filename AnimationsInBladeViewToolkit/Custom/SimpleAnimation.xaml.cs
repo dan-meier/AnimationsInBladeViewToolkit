8e095e8dfe4b0f71a7cd0faed58a267a338aca54
Dan Meier <Dan.Meier@FactorySoftworks.com>
2020-12-14 10:39:40 -0800

    Add project files.

---
+++ AnimationsInBladeViewToolkit/Custom/SimpleAnimation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace AnimationsInBladeViewToolkit.Custom
+{
+    public sealed partial class SimpleAnimation : UserControl
+    {
+        public SimpleAnimation()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void AnimateButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            AnimateButton.IsEnabled = false; // Disable the button while the animation is in progress
+
+
+            Element.Visibility = (Element.Visibility == Visibility.Visible) ? Visibility.Collapsed : Visibility.Visible;
+
+
+            await Task.Delay( 1000 );
+            AnimateButton.Content   = (string)AnimateButton.Content == "Fly In" ? "Fly Out" : "Fly In";
+            AnimateButton.IsEnabled = true; // Re-enable the button when the animation is complete
+        }
+    }
+}
---
+++ AnimationsInBladeViewToolkit/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace AnimationsInBladeViewToolkit
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void ShowButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ImplicitAnimationsBladeView.Visibility = Visibility.Visible;
+            ImplicitAnimationsBladeItem.IsOpen     = true;
+
+            ShowButton.IsEnabled  = false;
+            PageControl.IsEnabled = false;
+        }
+
+        private void HideButton_OnTapped( object sender, TappedRoutedEventArgs e )
+        {
+            ImplicitAnimationsBladeItem.IsOpen     = false;
+            ImplicitAnimationsBladeView.Visibility = Visibility.Collapsed;
+
+            ShowButton.IsEnabled  = true;
+            PageControl.IsEnabled = true;
+        }
+    }
+}
